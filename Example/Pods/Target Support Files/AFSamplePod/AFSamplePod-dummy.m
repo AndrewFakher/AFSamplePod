9c21a856f19f52214fb8c78b84b468a296b9103d
Andrew <andrew@Andrews-MacBook-Pro.local>
2019-10-16 22:21:26 +0200

    Initial commit with pod lib template

---
+++ Example/Pods/Target Support Files/AFSamplePod/AFSamplePod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AFSamplePod : NSObject
+@end
+@implementation PodsDummy_AFSamplePod
+@end
---
+++ Example/Pods/Target Support Files/Pods-AFSamplePod_Tests/Pods-AFSamplePod_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AFSamplePod_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_AFSamplePod_Tests
+@end
